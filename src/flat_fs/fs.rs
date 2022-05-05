e101246d6523416246ea3a3d01c4130720a47e2f
mox692 <55653825+mox692@users.noreply.github.com>
2022-05-06 01:27:41 +0900

    update stack

--- src/flat_fs/fs.rs
+++ src/flat_fs/fs.rs
@@ -1,22 +1,16 @@
 use super::super::FS;
-use crate::{MemStorage, MyError,  Storage};
+use crate::{MemStorage, MyError, Storage, Stack};
 use std::{
     collections::HashMap, error::Error, fmt::Debug, hash::Hash, marker::PhantomData,
 };
 
-pub struct Stack {}
-impl Stack {
-    pub fn new() -> Self{
-        Self {}
-    }
-}
 /// ひとまず、StorageのkeyもvalueもString型で実装してみる
 pub struct FlatFS<K, V, I, E, S> {
     pub storage: S,
     // file名をキーとして、そのファイルのメタ情報を保持しているHash Table.
     pub file_map: HashMap<K, I>,
     // blockのindex - nodeInfo のHashTable
-    pub free_storage_stack: Stack,
+    pub free_storage_stack: Stack<I>,
     // さしあたり今の実装では、newするタイミングで固定のblock sizeを与える
     _marker_k: PhantomData<K>,
     _marker_v: PhantomData<V>,
@@ -32,6 +26,7 @@ pub struct FileNode<P, Q> {
 // 以下は具体実装
 //
 
+// Iの具体実装.
 // offsetはblock indexを表す.
 impl FileNode<usize, usize> {
     pub fn new(offset: usize, size: usize) -> Self {
@@ -40,28 +35,39 @@ impl FileNode<usize, usize> {
             size: size,
         }
     }
+    pub fn new_vec_from_block_size(block_size: usize, block_num: usize) -> Vec<Self> {
+        let mut vec:Vec<Self> = vec![Self::default();block_num];
+        let mut cur_off = 0;
+        for i in 0..block_num {
+            let n = FileNode::new(cur_off, block_size);
+            vec[i] = n;
+            cur_off += block_size;
+        }
+        vec
+    }
 }
 
-impl<K, E, I> FlatFS<K, Vec<u8>, I, E, MemStorage<usize, usize, Vec<u8>, MyError>> {
+impl Default for FileNode<usize, usize> {
+    fn default() -> Self {
+        Self { offset: 0, size: 0}
+    }
+}
+
+impl<K, E> FlatFS<K, Vec<u8>, FileNode<usize, usize>, E, MemStorage<usize, usize, Vec<u8>, MyError>> {
     // MEMO: ひとまず固定.
     const BLOCK_SIZE: usize = 1000;
     pub fn new(s: MemStorage<usize, usize, Vec<u8>, MyError>) -> Self {
         let block_num = MemStorage::STORAGE_SIZE / Self::BLOCK_SIZE;
+        let fnodes = FileNode::new_vec_from_block_size(Self::BLOCK_SIZE, block_num);
         Self {
             storage: s,
             // ファイル名 - Filenode
             file_map: HashMap::new(),
-            free_storage_stack: Stack::new(),
+            free_storage_stack: Stack::new_with_init_data(fnodes),
             _marker_e: PhantomData,
             _marker_k: PhantomData::<K>,
             _marker_v: PhantomData::<Vec<u8>>,
         }
-    }
-
-    // TODO: 新しく空いてる領域を探してnodeを作成
-    // stackの先頭から空いてるindexを取ってくる
-    fn new_file_node(&self) -> Result<FileNode<usize, usize>, Box<dyn Error>> {
-        Ok(FileNode::new(0, 0))
     }
 }
 
@@ -78,12 +84,20 @@ where
 {
     // file_nameが存在しなかったら、そのファイルを新規で作成
     fn write(&mut self, file_name: K, contents: Vec<u8>) -> Result<(), Box<dyn Error>> {
+        // let a = self.file_map.get()
+        // match a {
+        //
+        //        }
         let info = match self.file_map.get(&file_name) {
             // TODO: hashmapから取得するときcloneすることをどこかに明記する
             | Some(i) => i.clone(),
-            | None => match self.new_file_node() {
-                | Err(e) => return Err(Box::<dyn Error>::from(e)),
-                | Ok(n) => n,
+            | None => {
+                let (s, n) = self.free_storage_stack.clone().pop();
+                self.free_storage_stack = s;
+                match n {
+                    Some(v) => v,
+                    None => return Err(Box::<dyn Error>::from(MyError::with_msg("no free disk stace found!!".to_string()))),
+                }
             },
         };
         self.storage.write(info.offset, contents)
